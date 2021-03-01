af84841506fbc5e9ea613b3db9ab3507a847fd2e
kknitteldev <79843809+kknitteldev@users.noreply.github.com>
2021-03-01 16:51:12 +0100

    Add files via upload

---
+++ assets/script/script.js
@@ -0,0 +1,18 @@
+function login() {
+    let tel = document.getElementById('tel');
+    let cp = document.getElementById('cp');
+    let pwd = document.getElementById('pwd');
+    let pwdConf = document.getElementById('pwdConf');
+    
+    if (tel.value.length != 10) {
+        alert('Erreur : le n° de téléphone n\'est pas conforme');
+    }
+    
+    if (cp.value.length != 5) {
+        alert('Erreur : le code postal n\'est pas conforme');
+    }
+    
+    if (pwd.value != pwdConf.value) {
+        alert('Erreur : les mots de passe ne correspondent pas');
+    }
+}
